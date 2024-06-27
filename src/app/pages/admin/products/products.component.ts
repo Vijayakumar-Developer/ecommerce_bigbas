ea88aa766d4db812857a3a76c4f2851a2d715808
Vijayakumar-Developer <vijai1993@gmail.com>
2024-06-27 18:06:40 +0530

    category api created

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  isSidePanelVisible :boolean =false;
+  productObj:any={
+    
+      "productId": 0,
+      "productSku": "",
+      "productName": "",
+      "productPrice": 0,
+      "productShortName": "",
+      "productDescription": "",
+      "createdDate": new Date(),
+      "deliveryTimeSpan": "",
+      "categoryId": 0,
+      "productImageUrl": ""
+  
+  }
+
+  categoryList : any [] =[];
+
+  constructor(private productSrv: ProductService){}
+
+  ngOnInit(): void {
+    this.getAllCategory();
+    
+  }
+  getAllCategory(){
+    this.productSrv.getCategory().subscribe((res:any)=>{
+      this.categoryList =res.data;
+
+    })
+  }
+
+  openSidePanel(){
+   // console.log(this.isSidePanelVisible);
+    this.isSidePanelVisible = true;
+  }
+  closeSidePanel(){
+    this.isSidePanelVisible = false;
+  }
 }
